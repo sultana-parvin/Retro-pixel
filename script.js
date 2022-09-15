816c405f57c9c72206f3ff86f5cba2fe70cf01aa
unknown <sultana.onlineformapro@gmail.com>
2022-09-15 14:08:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+gsap.fromTo('#bitcoin', {
+    x:-110, 
+    y:-800, rotate:95}, 
+    {
+    x:-45, 
+    y:-445, rotate:5, duration:20});
+
+gsap.fromTo('#grogo', {
+    x:-120, 
+    y:-850, rotate:45}, 
+    {
+    x:-56, 
+    y:-391, delay: 0.5, rotate:0, duration:18});
+    
+gsap.fromTo('#bomb', {
+    x:-100, 
+    y:-900, rotate: 150}, 
+    {
+    x:-182, 
+     y:-378, delay:1,  rotate:0, duration:15});
+
+gsap.fromTo('#baloon', {
+     x:-120, 
+    y:-1000,  rotate:352}, 
+    {
+    x:-270, 
+    y:-294, delay:0.5,  rotate:0, duration:12});
+     
+gsap.fromTo('#mac', {
+    x:-120, 
+    y:-950, rotate: 150}, 
+    {
+    x:-224, 
+    y:-231, delay:1, rotate:0, duration:12});
+
+
+     
+//    for( let i = 0 ; i < 100 ; i++) {
+//     let Star = document.createElement('div')
+//     let box = document.querySelector('#box')
+//     Star.classList.add('star')
+//     box.appendChild(Star)
+    
+//    }
+//    for( let i = 0; i<100 ; i++) {
+//     let distance = 0;
+//     distance += 2;
+//     Star[i].style.left = "2px";
+//    }
+    
+   
